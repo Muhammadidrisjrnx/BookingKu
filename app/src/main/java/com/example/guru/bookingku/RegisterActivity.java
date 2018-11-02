762f77155ed880c63237792cc7f8a48f97a8face
ajisuryawan123 <kazumovie132@gmail.com>
2018-11-02 11:43:48 +0700

    Register new user

---
+++ app/src/main/java/com/example/guru/bookingku/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.example.guru.bookingku;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.facebook.CallbackManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class RegisterActivity extends AppCompatActivity {
+    SignInButton googleSignButton;
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInOptions signInOptions;
+    CallbackManager callbackManager;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        callbackManager = CallbackManager.Factory.create();
+        signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(RegisterActivity.this, signInOptions);
+        googleSignButton = findViewById(R.id.sign_in_button);
+        googleSignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent, 997);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 997) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String realName = account.getDisplayName();
+            String username = account.getGivenName() + account.getId();
+            String email = account.getEmail();
+            String userId = account.getId();
+            String avatar;
+            if(account.getPhotoUrl()!=null) {
+                avatar = account.getPhotoUrl().toString();
+            }else{
+                avatar = null;
+            }
+
+            //intent
+            Toast.makeText(getApplicationContext(),realName+" , "+username,Toast.LENGTH_LONG).show();
+            editor = pref.edit();
+            editor.putString("user",userId);
+            editor.putString("name",realName);
+            editor.putString("username",username);
+            editor.putString("email",email);
+            editor.putString("avatar",avatar);
+            editor.commit();
+
+            Intent intent = new Intent(getApplicationContext(), Home.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } catch (ApiException ignored) {
+        }
+    }
+}
