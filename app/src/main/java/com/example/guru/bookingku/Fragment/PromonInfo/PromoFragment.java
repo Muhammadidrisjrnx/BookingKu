60d5491412a338adbf161d223309e5c5df00d470
fikr-mobiledev <fikr.mobiledev@gmail.com>
2018-11-16 16:27:27 +0700

    fix error

--- app/src/main/java/com/example/guru/bookingku/Fragment/PromonInfo/PromoFragment.java
+++ app/src/main/java/com/example/guru/bookingku/Fragment/PromonInfo/PromoFragment.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class PromoFragment extends BaseFragment {
     ArrayList<Gambar> arrayku;
     public RecyclerView recycler_view_list_film;
-    public ArrayList<ResonWaktuFalse> listFilm = new ArrayList<>();
+    public ArrayList<Time> listFilm = new ArrayList<>();
     public SectionListDataAdapter adapterAllTipe;
     private Slider slider;
 
@@ -48,12 +48,9 @@ public class PromoFragment extends BaseFragment {
         Gambar g = new Gambar();
         g.setGambar("https://assets.materialup.com/uploads/dcc07ea4-845a-463b-b5f0-4696574da5ed/preview.jpg");
         arrayku.add(g);
-
-
         Gambar g2 = new Gambar();
         g2.setGambar("https://assets.materialup.com/uploads/20ded50d-cc85-4e72-9ce3-452671cf7a6d/preview.jpg");
         arrayku.add(g2);
-
         Gambar g3 = new Gambar();
         g3.setGambar("https://assets.materialup.com/uploads/76d63bbc-54a1-450a-a462-d90056be881b/preview.png");
         arrayku.add(g3);
@@ -73,25 +70,6 @@ public class PromoFragment extends BaseFragment {
             }
         }, 1500);
 
-
-
-        BookingService bookingService = BookingClient.getRetrofit().create(BookingService.class);
-        Call<List<ResonWaktuFalse>> call = bookingService.dataWaktu();
-        call.enqueue(new Callback<List<ResonWaktuFalse>>() {
-            @Override
-            public void onResponse(Call<List<ResonWaktuFalse>> call, Response<List<ResonWaktuFalse>> response) {
-
-                Log.d("makanan", "onResponse: "+response.body().toString());
-            }
-
-            @Override
-            public void onFailure(Call<List<ResonWaktuFalse>> call, Throwable t) {
-                Log.e("TAG", "onFailure: " + t.getMessage());
-
-
-            }
-        });
-
         recycler_view_list_film = (RecyclerView) view.findViewById(R.id.recycler_view_list_film2);
         recycler_view_list_film.setHasFixedSize(true);
         adapterAllTipe = new SectionListDataAdapter(view.getContext(), listFilm);
@@ -99,5 +77,39 @@ public class PromoFragment extends BaseFragment {
         recycler_view_list_film.setAdapter(adapterAllTipe);
         recycler_view_list_film.setNestedScrollingEnabled(false);
         recycler_view_list_film.setVisibility(View.VISIBLE);
+        BookingService bookingService = BookingClient.getRetrofit().create(BookingService.class);
+//        Call<List<ResonWaktuFalse>> call = bookingService.dataWaktu();
+//        call.enqueue(new Callback<List<ResonWaktuFalse>>() {
+//            @Override
+//            public void onResponse(Call<List<ResonWaktuFalse>> call, Response<List<ResonWaktuFalse>> response) {
+//
+//                Log.d("makanan", "onResponse: "+response.body().toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<ResonWaktuFalse>> call, Throwable t) {
+//                Log.e("TAG", "onFailure: " + t.getMessage());
+//
+//
+//            }
+//        });
+        Call<ResonWaktuFalse> call = bookingService.dataWaktu();
+        call.enqueue(new Callback<ResonWaktuFalse>() {
+            @Override
+            public void onResponse(Call<ResonWaktuFalse> call, Response<ResonWaktuFalse> response) {
+                try{
+                    listFilm.addAll(response.body().getTime());
+                    adapterAllTipe.notifyDataSetChanged();
+                }catch (Exception e){
+                    Toast.makeText(getActivity(), "Something wrong is happen", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResonWaktuFalse> call, Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(getActivity(), t.getMessage() , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
