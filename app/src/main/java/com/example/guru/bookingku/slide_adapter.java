a974f5737c70a90c0edb0bbbd55b25be1b708994
ajisuryawan123 <kazumovie132@gmail.com>
2018-11-27 11:40:34 +0700

    makan

--- app/src/main/java/com/example/guru/bookingku/slide_adapter.java
+++ app/src/main/java/com/example/guru/bookingku/slide_adapter.java
@@ -2,6 +2,7 @@ package com.example.guru.bookingku;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +21,10 @@ public class slide_adapter extends PagerAdapter {
         this.context = context;
     }
 
-    public int[] image_slider = {R.drawable.wajah, R.mipmap.ic_launcher_round};
-    public String[] name_slider = {"Face", "Slide 2"};
-    public String[] description_slider = {"Lorem ipsum", "Lorem ipsum2"};
+    public int[] image_slider = {R.drawable.wajahfix, R.drawable.kuku2,R.drawable.aroma,R.drawable.potong,R.drawable.pedicure};
+    public int[] name_slider = {R.string.face,R.string.massage,R.string.nail,R.string.hair,R.string.welcome};
+    public int[] description_slider = {R.string.description_face, R.string.description_massage,R.string.description_nail,R.string.description_hair,R.string.empty};
+    //public int[]layout = {R.color.blue,R.color.green,R.color.blue,R.color.green,R.color.blue};
 
     @Override
     public int getCount() {
@@ -40,12 +42,16 @@ public class slide_adapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View view1 = layoutInflater.inflate(R.layout.slide_layout, container, false);
+        RelativeLayout relativeLayout = (RelativeLayout)view1.findViewById(R.id.relative_layout_slide);
         ImageView image_slide =(ImageView)view1.findViewById(R.id.image_slide);
         TextView text_view_slide = (TextView)view1.findViewById(R.id.text_view_slide);
         TextView text_view_description_slide = (TextView)view1.findViewById(R.id.text_view_description_slide);
+        //relativeLayout.setBackgroundColor(ContextCompat.getColor(context,layout[position]));
         image_slide.setImageResource(image_slider[position]);
         text_view_slide.setText(name_slider[position]);
         text_view_description_slide.setText(description_slider[position]);
+//        Animation animasi2 = AnimationUtils.loadAnimation(context,R.anim.coba);
+//        image_slide.startAnimation(animasi2);
         container.addView(view1);
         return view1;
     }
@@ -53,6 +59,6 @@ public class slide_adapter extends PagerAdapter {
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((RelativeLayout)object);
-        super.destroyItem(container, position, object);
+        //super.destroyItem(container, position, object);
     }
 }
